5fec51a8ece7e453b6fa512bdb791a12bfeed0ff
Ole Walberg <greblavelo@gmail.com>
2023-07-04 14:26:12 +0200

    Move while loop lesson into its own file

--- src/lessons/loops/loops.cpp
+++ src/lessons/loops/loops.cpp
@@ -63,24 +63,3 @@ void requestLoopInput()
 	}
 	printf("\n");
 }
-
-void printDoWhileLoop()
-{
-	bool willDoWhileLoopTerminate = false;
-	int iterations = 0;
-	do
-	{
-		system("clear");
-		printf("This is a Do While loop that will run indefinitely until a condition is met\n\n");
-		printf("We are currently on iteration: %i\n\n", iterations);
-		string input = requestInput("To meet the condition and break the loop write 'q'\nOr write whatever to do another iteration:");
-
-		if (input == "q")
-		{
-			willDoWhileLoopTerminate = true;
-			system("clear");
-		}
-
-		iterations++;
-	} while (!willDoWhileLoopTerminate);
-}
